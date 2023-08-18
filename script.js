1ec212b8151cc31df1d2716cb58b7d3d08b19836
Anna Ostrowska <ana.os@wp.pl>
2023-08-18 19:12:52 +0200

    bootstrap fixes

--- script.js
+++ script.js
@@ -12,17 +12,14 @@ function loadHTML(url, elementId, callback) {
     xhr.send();
 }
 
-loadHTML('sections/intro/intro.html', 'intro-content-placeholder', function() {
-    loadHTML('sections/offer/offer.html', 'offer-content-placeholder', function() {
-        loadHTML('sections/company/company.html', 'company-content-placeholder');
+loadHTML('sections/navbar/navbar.html', 'navbar-content-placeholder', function() {
+    loadHTML('sections/intro/intro.html', 'intro-content-placeholder', function() {
+        loadHTML('sections/offer/offer.html', 'offer-content-placeholder', function() {
+            loadHTML('sections/company/company.html', 'company-content-placeholder', function() {
+                loadHTML('sections/realizations/realizations.html', 'realizations-content-placeholder');
+            });
+        });
     });
 });
 
-// Pobierz element karuzeli
-var imageCarousel = document.getElementById('imageCarousel');
 
-// Inicjalizuj karuzelę za pomocą Bootstrap
-var carousel = new bootstrap.Carousel(imageCarousel, {
-    interval: 5000, // Czas między zmianami slajdów (w milisekundach)
-    pause: 'hover' // Pauzowanie karuzeli po najechaniu myszą
-});
